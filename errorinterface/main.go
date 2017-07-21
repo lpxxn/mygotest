b01d0798f477c8bcf4a3327a4d12fdb63e5970c5
li.p <li.p@angaomeng.com>
2017-07-21 17:24:23 +0800

    error interface

---
+++ errorinterface/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+type MyErr struct {
+	When time.Time
+	What string
+}
+
+func(e *MyErr) Error() string {
+	return fmt.Sprintf("at %v, %s", e.When, e.What)
+}
+
+func run() error {
+	return &MyErr{
+		time.Now(),
+		"it didn't work",
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
