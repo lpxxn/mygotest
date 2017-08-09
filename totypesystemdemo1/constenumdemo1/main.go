08a618dc2fe3fd19f7b51c17b0804542ea13899a
li.p <li.p@angaomeng.com>
2017-08-09 11:43:47 +0800

    float parse

--- totypesystemdemo1/constenumdemo1/main.go
+++ totypesystemdemo1/constenumdemo1/main.go
@@ -16,6 +16,20 @@ const (
 	h
 )
 
+type ByteSize float64
+
+const (
+	B           = iota + 1                  // ignore first value by assigning to blank identif
+	KB ByteSize = 1 << (10 * iota) // 1 << (10*1)
+	MB                                   // 1 << (10*2)
+	GB                                   // 1 << (10*3)
+	TB                                   // 1 << (10*4)
+	PB                                   // 1 << (10*5)
+	EB                                   // 1 << (10*6)
+	ZB                                   // 1 << (10*7)
+	YB                                   // 1 << (10*8)
+)
+
 type MyIntEnum int
 const (
 	i MyIntEnum = iota
@@ -23,6 +37,8 @@ const (
 )
 
 func main() {
+
+	fmt.Println(B, KB, MB, GB)
 	fmt.Println(a, b, c)
 	fmt.Println(e, " g: ", g)
 
---
+++ totypesystemdemo1/consttestdemo2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type ByteSize uint64
+
+const (
+	B  ByteSize = 1 + iota
+	KB          = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+
+	fnUnmarshalText string = "UnmarshalText"
+	maxUint64       uint64 = (1 << 64) - 1
+	cutoff          uint64 = maxUint64 / 10
+)
+
+
+
+
+func main() {
+	fmt.Println(B, KB, MB, TB)
+	str := "hello world!";
+	bytes := []byte(str)
+	var len_bytes = len(bytes)
+	fmt.Println(len_bytes, bytes);
+	 c := float64(len_bytes) / float64(KB)
+	svalue := strconv.FormatFloat(c, 'f', 3, 32)
+	svalue2 := fmt.Sprintf("%.3f", c)
+	 f, err := strconv.ParseFloat(svalue, 64)
+	fmt.Println(svalue, svalue2, f, err)
+	fmt.Println("kilobyte", len_bytes / KB, c)
+}
