022745fa73114c840c8585d8132c9b5506fe3912
li.p <li.p@angaomeng.com>
2017-08-09 17:46:25 +0800

    read and write json to file

---
+++ workspace/publishcrm/serverconfig/configjsonstruct.go
@@ -0,0 +1,43 @@
+package serverconfig
+
+import (
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+	"os"
+)
+
+type CrmConfig struct {
+	JsonFilePath string `json:"jsonFile"`
+	Name string `json:"name"`
+}
+
+func ReadCrmConfig(path string) (*CrmConfig, error) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Read File Error the Path : %s, Err: %v", path, err)
+		return nil, err
+	}
+	var jsonObj *CrmConfig = &CrmConfig{}
+
+	if err = json.Unmarshal(file, jsonObj); err != nil {
+		fmt.Printf("Unmarsh Json Error : %v", err)
+		return nil, err
+	}
+
+	return jsonObj, nil
+}
+
+func (config *CrmConfig) SaveToFile(path string) error {
+	json, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+	perm := os.FileMode(0777)
+	err = ioutil.WriteFile(path, json, perm)
+
+	if err != nil {
+		return err;
+	}
+	return nil
+}
