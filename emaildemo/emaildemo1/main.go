71161c248cc91340dacf3fe61eb0914dc18e3956
li.p <li.p@angaomeng.com>
2017-09-18 16:30:59 +0800

    test

--- emaildemo/emaildemo1/main.go
+++ emaildemo/emaildemo1/main.go
@@ -17,7 +17,7 @@ func main() {
 	m.SetHeader("From", "p.li@angaomeng.com")
 	m.SetHeader("To", "lpxxn@foxmail.com", "mi_duo@126.com", "p.li@angaomeng.com")
 	m.SetHeader("Subject", "Test")
-	m.SetBody("text/html", "Hello <b>你好</b> and <i>我要李鹏</i>测试成功!")
+	m.SetBody("text/html", "Hello <b>你好</b> and <i>我是李鹏</i>测试成功!")
 
 	// Send the email to Bob, Cora and Dan.
 	if err := d.DialAndSend(m); err != nil {
