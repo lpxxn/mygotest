658a77c0d327315129c2c2f508d2db97b02716d7
li.p <li.p@angaomeng.com>
2017-09-18 16:29:21 +0800

    test

---
+++ emaildemo/emaildemo1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"crypto/tls"
+
+	// go get gopkg.in/gomail.v2
+	// donot support dep command
+	"gopkg.in/gomail.v2"
+)
+
+func main() {
+	d := gomail.NewDialer("smtp.exmail.qq.com", 465, "p.li@angaomeng.com", "AgmLip123p.li")
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", "p.li@angaomeng.com")
+	m.SetHeader("To", "lpxxn@foxmail.com", "mi_duo@126.com", "p.li@angaomeng.com")
+	m.SetHeader("Subject", "Test")
+	m.SetBody("text/html", "Hello <b>你好</b> and <i>我要李鹏</i>测试成功!")
+
+	// Send the email to Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+
+}
+
