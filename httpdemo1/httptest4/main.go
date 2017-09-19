eda7cc7edf8209538d7fc9e4380041668c0f0d62
lpxxn <mi_duo@live.com>
2017-09-19 15:05:34 +0800

    test

--- httpdemo1/httptest4/main.go
+++ httpdemo1/httptest4/main.go
@@ -19,13 +19,19 @@ type JdPrice []struct {
 	P  string `json:"p"`
 }
 
-func main() {
-	c := cron.New()
-	c.AddFunc("*/1 * * * * *", func() { fmt.Println("Every hour on the half hour") })
-	c.Start()
-	defer c.Stop()
+const (
+	priceUrl = "https://p.3.cn/prices/mgets?skuIds=J_"
+)
+
+var favoritesProduct = map[string]float64{
+	"2952697": 559,
+	"2316993": 333,
+}
+
+func GetPrice(product string, myPrice float64) {
 	//2316993  2316993
-	resp, err := http.Get("https://p.3.cn/prices/mgets?skuIds=J_2316993")
+	//resp, err := http.Get("https://p.3.cn/prices/mgets?skuIds=J_2316993")
+	resp, err := http.Get(priceUrl + product)
 	if err == nil {
 		msg, err := ioutil.ReadAll(resp.Body)
 		if err == nil {
@@ -40,6 +46,17 @@ func main() {
 	} else {
 		fmt.Println(err)
 	}
+}
+
+func main() {
+	c := cron.New()
+	c.AddFunc("*/1 * * * * *", func() { fmt.Println("Every hour on the half hour") })
+	c.Start()
+	defer c.Stop()
+
+	for pro, price := range favoritesProduct {
+		go GetPrice(pro, price)
+	}
 
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
@@ -50,5 +67,5 @@ func main() {
 	}
 
 	// single select can block the app
-	select {}
+	//select {}
 }
