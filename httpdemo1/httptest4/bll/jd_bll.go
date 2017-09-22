09dcf33fce70f0c7e637f7dc9adfdb7540bcba2a
li.p <li.p@angaomeng.com>
2017-09-22 09:57:25 +0800

    test

--- httpdemo1/httptest4/bll/jd_bll.go
+++ httpdemo1/httptest4/bll/jd_bll.go
@@ -23,22 +23,26 @@ func GetJdPrice(url string, product *models.JdFavoriteProduct) {
 		msg, _ := ioutil.ReadAll(res.Body)
 		var jd models.JdPrice
 		json.Unmarshal(msg, &jd)
+		if jd == nil || len(jd) == 0 {
+			fmt.Println("京东接口出现问题，没有返回数据")
+			return
+		}
 		t := time.Now()
 		fmt.Printf("京东 商品 %s,当前价格  %s, 期望价格:%g ,编号：%s \n", product.ProductName, jd[0].P, product.FavoritePrice, product.ProductCode)
 		if product.SendCount > 0 {
 			fmt.Printf("have send %d \n", product.SendCount)
 			return
 		}
-		if jd != nil && len(jd) > 0  {
-			p, _ :=strconv.ParseFloat(jd[0].P, 32)
-			pf := float32(p)
-			if p > 0 && pf <= product.FavoritePrice {
-				fmt.Println("send email")
-				go utils.SendEmail(fmt.Sprintf("京东 商品<b> %s </b>,当前价格 <b> %s </b>, 期望价格<b>:%g </b>, 编号：%s ", product.ProductName, jd[0].P, product.FavoritePrice, product.ProductCode))
-				product.SendCount += 1
-				product.SendEmailTime = t
-			}
+
+		p, _ :=strconv.ParseFloat(jd[0].P, 32)
+		pf := float32(p)
+		if p > 0 && pf <= product.FavoritePrice {
+			fmt.Println("send email")
+			go utils.SendEmail(fmt.Sprintf("京东 商品<b> %s </b>,当前价格 <b> %s </b>, 期望价格<b>:%g </b>, 编号：%s ", product.ProductName, jd[0].P, product.FavoritePrice, product.ProductCode))
+			product.SendCount += 1
+			product.SendEmailTime = t
 		}
+
 	} else {
 		fmt.Errorf("error response")
 	}
