116a93743b83d0183d872bd6d2c6defe99b02d83
lpxxn <mi_duo@live.com>
2017-11-25 18:39:35 +0800

    test

--- workspace/webdemo2/crmweb.go
+++ workspace/webdemo2/crmweb.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mygotest/workspace/webdemo2/utils"
+)
 
+func main() {
+	_, err := utils.Cluster.Set("crmweb", "value111", 0).Result()
+	fmt.Println(err)
 }
--- workspace/webdemo2/utils/redishelp.go
+++ workspace/webdemo2/utils/redishelp.go
@@ -1,10 +1,41 @@
 package utils
 
-import "github.com/go-redis/redis"
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+	"github.com/mygotest/workspace/webdemo2/utils/zaplogger"
+	"time"
+)
 
-var Cluster *redis.ClusterClient
+//var Cluster *redis.ClusterClient = nil
+var Cluster *redis.Client = nil
 
+func init() {
+	ConnectCluster()
+}
 func ConnectCluster() {
-	address := ""
-
+	//Cluster = redis.NewClusterClient(&redis.ClusterOptions{
+	//	Addrs:        []string{"192.168.0.105:6379"},
+	//	PoolSize:     1000,
+	//	PoolTimeout:  2 * time.Minute,
+	//	IdleTimeout:  10 * time.Minute,
+	//	ReadTimeout:  2 * time.Minute,
+	//	WriteTimeout: 1 * time.Minute,
+	//	// Password: password,
+	//})
+	Cluster = redis.NewClient(&redis.Options{
+		Addr:         "192.168.0.105:6379",
+		Password:     "",
+		PoolSize:     1000,
+		PoolTimeout:  2 * time.Minute,
+		IdleTimeout:  10 * time.Minute,
+		ReadTimeout:  2 * time.Minute,
+		WriteTimeout: 1 * time.Minute,
+		//DB:           0,
+	})
+	_, err := Cluster.Ping().Result()
+	if err != nil {
+		fmt.Println(err)
+		zaplogger.InitLogger().Panic("Redis error")
+	}
 }
