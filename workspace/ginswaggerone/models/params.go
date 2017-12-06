c02d5d932e28d13d05754b035c267c4c7bfb4468
lpxxn <mi_duo@live.com>
2017-12-06 14:35:39 +0800

    test swagger

--- workspace/ginswaggerone/models/params.go
+++ workspace/ginswaggerone/models/params.go
@@ -7,3 +7,29 @@ type P1Req struct {
 	Age     int       `json:"age" form:"age"`
 	CurTime time.Time `json:"cur_time" form:"cur_time"`
 }
+
+// @hello
+// yo yo yo yo
+type Pet struct {
+	ID       int `json:"id"`
+	Category struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"category"`
+	Name      string   `json:"name"`
+	PhotoUrls []string `json:"photoUrls"`
+	Tags      []struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"tags"`
+	Status string `json:"status"`
+}
+
+type Pet2 struct {
+	ID int `json:"id"`
+}
+
+type APIError struct {
+	ErrorCode    int
+	ErrorMessage string
+}
