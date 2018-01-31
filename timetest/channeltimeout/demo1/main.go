c57dd8138fcec1b58eb880005c49182015d5d94a
lpxxn <mi_duo@live.com>
2018-01-31 22:33:50 +0800

    test1

--- timetest/channeltimeout/demo1/main.go
+++ timetest/channeltimeout/demo1/main.go
@@ -1,1 +1,37 @@
-package demo1
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func check(u string, checked chan<- bool) {
+	time.Sleep(2 * time.Second)
+
+	checked <- true
+}
+
+func IsReachable(urls []string) bool {
+	ch := make(chan bool, 1)
+
+	for _, url := range urls {
+		go func(u string) {
+			checked := make(chan bool)
+			go check(u, checked)
+
+			select {
+			case ret := <-checked:
+				ch <- ret
+
+			case <-time.After(2 * time.Second):
+				ch <- false
+			}
+
+		}(url)
+	}
+	return <-ch
+}
+
+func main() {
+	fmt.Println(IsReachable([]string{"url1", "url2"}))
+}
