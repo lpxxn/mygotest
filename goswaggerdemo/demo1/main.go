56fd7b0895d7cc55c81a91b56c2ff8d287d62570
lpxxn <mi_duo@live.com>
2018-02-06 17:58:03 +0800

    test

--- goswaggerdemo/demo1/main.go
+++ goswaggerdemo/demo1/main.go
@@ -1,1 +1,21 @@
-package demo1
+package main
+
+import "time"
+
+// swagger:model
+type Person struct {
+	// Their preferred name
+	Name string
+	// Their email
+	//
+	// swagger:strfmt email
+	Email string
+	// When the registered 自动转换成 string
+	RegistrationDate time.Time
+}
+
+func main() {
+	print(1)
+}
+
+// swagger generate spec -m tmp.go -o swagger.json
