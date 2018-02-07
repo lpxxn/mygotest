663408910f48321409a558173d76d02b84a356cd
lpxxn <mi_duo@live.com>
2018-02-07 19:26:09 +0800

    test swagger post

--- workspace/webdemo2/server/router.go
+++ workspace/webdemo2/server/router.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"github.com/mygotest/workspace/webdemo2/models"
 	"github.com/mygotest/workspace/webdemo2/server/userinfo"
 	"net/http"
@@ -26,6 +27,7 @@ func userRouter(r *gin.Engine) {
 	r.GET("ping", ping)
 	r.POST("setuserinfo", userinfo.SetUserInfo)
 	r.GET("getuserinfo", userinfo.GetUserInfo)
+	r.GET("pets", listPets)
 }
 
 func apiRouter(r *gin.Engine) {
@@ -33,6 +35,47 @@ func apiRouter(r *gin.Engine) {
 	api.GET("pingapi", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"apiMsg": "Hello Api!"})
 	})
+}
+
+// swagger:parameters listPets
+type PetQueryFlags struct {
+	// Status
+	Status string `json:"status"`
+	Id     string `json:"id"`
+}
+
+// A GenericError is the default error message that is generated.
+// For certain status codes there are more appropriate error structures.
+//
+// swagger:response genericError
+type GenericError struct {
+	// in: body
+	Body struct {
+		Code    int32 `json:"code"`
+		Message error `json:"message"`
+	} `json:"body"`
+}
+
+// GetPets swagger:route GET /pets pets listPets
+//
+// Lists the pets known to the store.
+//
+// By default it will only lists pets that are available for sale.
+// This can be changed with the status flag.
+//
+// Responses:
+// 		default: genericError
+// 		    200: []pingResponse
+func listPets(c *gin.Context) {
+	strpar := c.DefaultQuery("status", "1")
+	// param in path
+	strname := c.Param("id")
+	fmt.Println(strpar)
+	fmt.Println(strname)
+	resp := make([]models.RspPing, 0)
+	resp = append(resp, models.RspPing{Msg: "abcdef"})
+	c.JSON(http.StatusOK, resp)
+
 }
 
 // swagger:route GET /ping pets users listPets
