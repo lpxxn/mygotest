9c6a418c35774a3029dc687afdebbd0e640dd873
lpxxn <mi_duo@live.com>
2018-03-12 15:15:22 +0800

    test

---
+++ goalgorithms/fibonacci/demo2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func fibonacci(i int) int{
+	if i == 0 {
+		return 0
+	}
+	if i < 2 {
+		return 1
+	}
+	 rev := fibonacci(i - 1) + fibonacci(i - 2)
+	 return rev
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(fibonacci(i))
+	}
+}
