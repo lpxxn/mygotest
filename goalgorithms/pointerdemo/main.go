ea19b6f1db81d43826314c18423f7809292133d5
lpxxn <mi_duo@live.com>
2018-03-21 21:54:14 +0800

    test nil interface

--- goalgorithms/pointerdemo/main.go
+++ goalgorithms/pointerdemo/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func Foo(x interface{}) {
 	if x == nil {
@@ -10,12 +13,49 @@ func Foo(x interface{}) {
 	fmt.Println("non-empty interface")
 }
 
+func Foo2(x interface{}) {
+	if reflect.ValueOf(x).IsNil() {
+		fmt.Println("x is empty interface")
+		return
+	}
+	fmt.Println("x is non-empty interface")
+}
+
 
 func A() *int {
 	return nil
 }
 
+func B() *int {
+	var i *int
+	return i
+}
+
+type T struct {
+
+}
+
+func M() *T {
+	var t *T
+	return t
+}
+
 func main() {
 	a := A()
+	if a == nil {
+		fmt.Println("a is nil")
+	}
+
 	Foo(a)
+	Foo2(a)
+
+	b := B()
+	Foo(b)
+	Foo2(b)
+
+	t1:= M()
+	if t1 == nil {
+		fmt.Println("t1 is nil")
+	}
+
 }
