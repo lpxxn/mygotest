552d123acf51790dc602ab2bccf7191a603c2417
lpxxn <mi_duo@live.com>
2018-04-03 13:19:28 +0800

    test

--- mongodbdemo/crondemo1/main.go
+++ mongodbdemo/crondemo1/main.go
@@ -1,1 +1,32 @@
-package crondemo1
+package main
+
+import (
+	"fmt"
+	"github.com/robfig/cron"
+	"time"
+)
+func main() {
+	cron1 := cron.New()
+	defer cron1.Stop()
+
+	// 秒
+	cron1.AddFunc("*/1 * * * * *", func() {
+		fmt.Println("second")
+		})
+
+	cron1.AddFunc("0 */30 * * * ?", func() {
+		fmt.Println("min 30")
+	})
+
+	// 晚上0点
+	cron1.AddFunc("0 0 0 * * ?", func() {
+		fmt.Println("zero")
+
+		fmt.Println(time.Now())
+		fmt.Println(time.Now().AddDate(0, 0, -1))
+	})
+	fmt.Println("begin")
+
+	cron1.Start()
+	select{}
+}
