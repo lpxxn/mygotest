1c30c706f85b2d0194bc739dd0ecffc149cb7b22
lpxxn <mi_duo@live.com>
2018-05-03 10:44:18 +0800

    test

--- httpdemo1/urltest/main.go
+++ httpdemo1/urltest/main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-	"net/url"
+	"encoding/json"
 	"fmt"
 	"github.com/axgle/mahonia"
-	"encoding/json"
+	"net/url"
 )
 
 type YpCbInfo []struct {
-	Sid             int64       `json:"sid"`
+	Sid             int64  `json:"sid"`
 	UID             string `json:"uid"`
-	UserReceiveTime string      `json:"user_receive_time"`
-	ErrorMsg        string      `json:"error_msg"`
-	Mobile          string      `json:"mobile"`
-	ReportStatus    string      `json:"report_status"`
+	UserReceiveTime string `json:"user_receive_time"`
+	ErrorMsg        string `json:"error_msg"`
+	Mobile          string `json:"mobile"`
+	ReportStatus    string `json:"report_status"`
 }
 
 func main() {
 	//fmt.Println(url.QueryEscape("+41545"))
 	//fmt.Println(url.QueryEscape("123456798"))
-
 
 	m := mahonia.NewDecoder("UTF-8")
 
@@ -28,7 +27,7 @@ func main() {
 	un, e := url.QueryUnescape(str)
 	fmt.Println(un, e)
 
-	un, e  = url.QueryUnescape(un)
+	un, e = url.QueryUnescape(un)
 	fmt.Println(un, e)
 	m.Translate([]byte(str), true)
 
@@ -43,7 +42,6 @@ func main() {
 
 	json.Unmarshal([]byte(cb_str), &cb_info)
 	fmt.Println(cb_info)
-
 
 	//str2 := `{"level":"info","ts":1525260979.8594625,"msg":"yp cb :sms_status=%255B%257B%2522sid%2522%253A23832105116%252C%2522uid%2522%253Anull%252C%2522user_receive_time%2522%253A%25222018-05-02%2B19%253A36%253A18%2522%252C%2522error_msg%2522%253A%2522DELIVRD%2522%252C%2522mobile%2522%253A%252215117950565%2522%252C%2522report_status%2522%253A%2522SUCCESS%2522%257D%255D","curenttime":"2018-05-02T19:36:19+08:00"}`
 	//un3, err := url.QueryUnescape(str2)
