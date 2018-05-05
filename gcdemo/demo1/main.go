82d46bd369888e94d14a2eda21ef837250af35a7
lpxxn <mi_duo@live.com>
2018-05-05 14:37:11 +0800

    gc channel

---
+++ gcdemo/demo1/main.go
@@ -0,0 +1,32 @@
+
+package main
+
+import (
+	"runtime"
+	"fmt"
+)
+
+func mk2() {
+	//b := new([10000]int64)
+	//_ = b
+	//println(b, "stored at", &b)
+
+	c := make(chan int64, 10000)
+	c <- 1
+}
+
+func mk1() { mk2() }
+
+func main() {
+	for i := 0; i < 10; i++ {
+		mk1()
+		RM()
+		runtime.GC()
+	}
+}
+
+func RM(){
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Println(m.Alloc)
+}
