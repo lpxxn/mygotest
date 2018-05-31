79e3aa5825978f2a2c7ceed0a9d31a5b35a1cdcc
lpxxn <mi_duo@live.com>
2018-05-31 15:11:31 +0800

    test

--- goredis/demo_hash/main.go
+++ goredis/demo_hash/main.go
@@ -24,6 +24,8 @@ func main() {
 
 	len, err := client.HLen("mb").Result()
 	fmt.Println("len: ", len)
+
+	multiplefields(client)
 }
 
 
@@ -39,4 +41,21 @@ func RClient() (*redis.Client, error) {
 	fmt.Println(pong, err)
 
 	return client, err
+}
+
+
+func multiplefields(client *redis.Client) {
+	client.HSet("user:1", "name", "li")
+	client.HSet("user:1", "age", 10)
+
+	client.HMSet("user:2", map[string]interface{}{"name": "peng", "age": 18})
+
+	u1, err := client.HGetAll("user:1").Result()
+	fmt.Println("u1: ", u1, "  err: ", err)
+
+	u2age, err := client.HGet("user:2", "age").Result()
+
+	fmt.Println("u2age: ", u2age, "  err: ", err)
+
+
 }
