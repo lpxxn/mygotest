f4a6a4756a7428ebfdd7540acdbf8858607d86f0
lpxxn <mi_duo@live.com>
2018-06-11 11:41:33 +0800

    byte append benchmark

--- jsoniterdemo/demo/json_test.go
+++ jsoniterdemo/demo/json_test.go
@@ -2,23 +2,49 @@ package main
 
 import (
 	"testing"
-	"github.com/json-iterator/go"
 	"encoding/json"
+	"github.com/json-iterator/go"
+	"time"
+	"fmt"
 )
 
 func Benchmark_Json(b *testing.B) {
 	//p := fmt.Println
 	val := []byte(`{"ID":1,"Name":"Reds","Colors":["Crimson","Red","Ruby","Maroon"]}`)
-
+	entity := make(map[string]interface{})
 	b.StopTimer()
 
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 
 		//jsoniter.Get(val, "Colors", 0).ToString()
-		jsoniter.Get(val, "Name").ToString()
+		//jsoniter.Get(val, "Name").ToString()
+		jsoniter.Unmarshal(val, &entity)
+		entity["time"] = time.Now().Format(time.RFC3339)
+		jsoniter.Marshal(entity)
+		//jsoniter.Get(val, "Type").ToString()
 	}
 
+}
+
+func Benchmark_ByteReplace(b *testing.B) {
+	//var rightC = []byte{'}'}
+	val := []byte(`{"ID":1,"Name":"Reds","Colors":["Crimson","Red","Ruby","Maroon"]}`)
+	lenval := len(val)
+	//entity := make(map[string]interface{})
+	b.StopTimer()
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+
+		newval := []byte(fmt.Sprintf(`,"Time":"%s"}`, time.Now().Format(time.RFC3339)))
+		nval := append(val[:lenval -1], newval...)
+		_ = jsoniter.Valid(nval)
+
+		//fmt.Println(isValid)
+		//bytes.Replace(val, rightC)
+	}
 }
 
 func Benchmark_GoJson(b *testing.B) {
@@ -34,5 +60,7 @@ func Benchmark_GoJson(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		json.Unmarshal(val, &entity)
 		//_ = entity["Name"]
+		entity["time"] = time.Now().Format(time.RFC3339)
+		json.Marshal(entity)
 	}
 }
--- jsoniterdemo/demo/main.go
+++ jsoniterdemo/demo/main.go
@@ -10,6 +10,8 @@ func main() {
 	val := []byte(`{"ID":1,"Name":"Reds","Colors":["Crimson","Red","Ruby","Maroon"]}`)
 	p(jsoniter.Get(val, "Colors", 0).ToString())
 	p(jsoniter.Get(val, "Name").ToString())
-
+	if jsoniter.Get(val, "Type").ToString() == "" {
+		p("empty")
+	}
 
 }
