b623fe8d8794f7228d8125fc7a80d82d64539ec0
lpxxn <mi_duo@live.com>
2018-06-26 14:22:14 +0800

    test context demo

---
+++ gocontextdemo/demo1/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	timeOut := 5 * time.Second
+	deadline := time.Now().Add(4 * time.Hour)
+	timeOutCtx, timeCancel := context.WithTimeout(context.Background(), timeOut)
+	defer timeCancel()
+
+	cancelCtx, cancelFunc := context.WithCancel(context.Background())
+
+	deadlineCtx, _ := context.WithDeadline(cancelCtx, deadline)
+
+	go contextDemo("[timecount]", timeOutCtx)
+
+	go contextDemo("[cancelContext]", cancelCtx)
+
+	go contextDemo("[deadlineContext]", deadlineCtx)
+
+
+	// wait for the timeout to expire
+	<- timeOutCtx.Done()
+
+
+	//go contextDemo("[cancelContext]", cancelCtx)
+	//
+	//go contextDemo("[deadlineContext]", deadlineCtx)
+
+	// cancel the deadline context as well as its child -- the cancelCtx
+	fmt.Println("Cancelling the cancel context.....")
+	cancelFunc()
+
+	<- cancelCtx.Done()
+	fmt.Println("The cancel context has been cancelled")
+
+	fmt.Println("wait for both contexts to be calcelled")
+	<- deadlineCtx.Done()
+	fmt.Println("the deadline context has been cancelled....")
+
+}
+
+
+func contextDemo(name string, ctx context.Context) {
+	deadline, ok := ctx.Deadline()
+	for {
+		if ok {
+			fmt.Println(name, "will exire at: ", deadline)
+		} else {
+			fmt.Println(name, "has no deadline")
+		}
+		time.Sleep(time.Second)
+	}
+}
