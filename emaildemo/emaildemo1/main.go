3a861cacc861598fe1ff3bd6fa5c18691ea68a49
lpxxn <mi_duo@live.com>
2018-08-02 14:44:42 +0800

    send email

--- emaildemo/emaildemo1/main.go
+++ emaildemo/emaildemo1/main.go
@@ -9,17 +9,24 @@ import (
 )
 
 func main() {
-	d := gomail.NewDialer("smtp.mxhichina.com", 465, "lipeng@rrzhuan.com", "")
+	d := gomail.NewDialer("smtp.mxhichina.com", 465, "lipeng@rrzhuan.com", "Rrnc15117950565")
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	s, err := d.Dial()
+	if err != nil {
+		panic(err)
+	}
+	defer s.Close()
 
 	m := gomail.NewMessage()
+	//m.SetHeader("From", "lipeng@rrzhuan.com")
 	m.SetHeader("From", "lipeng@rrzhuan.com")
 	m.SetHeader("To", "lpxxn@foxmail.com", "mi_duo@126.com", "zhangyanan@rrzhuan.com", "yejiani@rrzhuan.com")
 	m.SetHeader("Subject", "Test")
-	m.SetBody("text/html", "Hello <b>你好</b> and <i>我是李鹏</i>测试成功!")
+	//m.SetBody("text/html", "Hello <b>你好</b> and <i>测试测试</i>测试成功!")
+	m.SetBody("text/html", "<b>【RRNC】</b>测试邮箱")
 
 	// Send the email to Bob, Cora and Dan.
-	if err := d.DialAndSend(m); err != nil {
+	if err := gomail.Send(s, m); err != nil {
 		panic(err)
 	}
 
---
+++ pprofdemo/gindemo/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/DeanThompson/ginpprof"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	// automatically add routers for net/http/pprof
+	// e.g. /debug/pprof, /debug/pprof/heap, etc.
+	ginpprof.Wrap(router)
+
+	// ginpprof also plays well with *gin.RouterGroup
+	// group := router.Group("/debug/pprof")
+	// ginpprof.WrapGroup(group)
+
+	router.Run(":9919")
+}
+
+
+//go get github.com/DeanThompson/ginpprof
