b928d165f69285a8d4bb05b44c0b9aef0545ea42
lpxxn <mi_duo@live.com>
2018-10-10 10:14:05 +0800

    test 1

--- jwtdemo/demo1/main.go
+++ jwtdemo/demo1/main.go
@@ -10,12 +10,30 @@ var key = []byte("abcdefasdf")
 
 
 func main() {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	cToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"exp": time.Now().Add(time.Hour * 2),
 		"uid": 11111,
 	})
-	tokenStr, err := token.SignedString(key)
+	tokenStr, err := cToken.SignedString(key)
 	fmt.Print(tokenStr, err)
+
+	token, _ := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+
+			return nil, fmt.Errorf("not authorization")
+		}
+		return key, nil
+	})
+	if !token.Valid {
+		fmt.Println("false")
+	} else {
+		mapData := token.Claims.(jwt.MapClaims)
+		uid := mapData["uid"]
+		exp := mapData["exp"]
+		fmt.Println(uid, exp)
+	}
+	fmt.Println(token.Signature)
+
 }
 
 
