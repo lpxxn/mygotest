42880be8baed720269645bb85366e916feb178b8
lpxxn <mi_duo@live.com>
2018-11-20 11:58:13 +0800

    test http post api

--- gotest/httpdemo1/sample_test.go
+++ gotest/httpdemo1/sample_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 )
 
@@ -19,5 +21,28 @@ func TestNameHandler(t *testing.T) {
 	if len(body) == 0 {
 		t.Errorf("response is empty")
 	}
-	t.Logf("response body %s", body)
+	t.Logf("response body %s\n", body)
+}
+
+func TestSetName(t *testing.T) {
+	student := NewStudent("li")
+	handler := SetName(student)
+	params := url.Values{}
+	params.Set("name", "peng")
+
+	req, err := http.NewRequest("POST", "setName", strings.NewReader(params.Encode()))
+	if err != nil {
+		t.Errorf("new request error %v", err)
+	}
+	req.Header.Set("Content-Type", "multipart/form-data;")
+	w := httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("get name api error")
+	}
+	body := w.Body.String()
+	if len(body) == 0 {
+		t.Errorf("response is empty")
+	}
+	t.Logf("response body %s\n", body)
 }
