3e0fd043ed2507a99c0ab53ce5c2c6ffeb859d6d
lpxxn <mi_duo@live.com>
2018-11-21 09:50:21 +0800

    change string to already exists const value

--- gotest/httpdemo1/sample_test.go
+++ gotest/httpdemo1/sample_test.go
@@ -11,7 +11,7 @@ import (
 func TestNameHandler(t *testing.T) {
 	student := NewStudent("li")
 	handler := NameHandler(student)
-	req, _ := http.NewRequest("GET", "name", nil)
+	req, _ := http.NewRequest(http.MethodGet, "name", nil)
 	w := httptest.NewRecorder()
 	handler.ServeHTTP(w, req)
 	if w.Code != http.StatusOK {
@@ -30,7 +30,7 @@ func TestSetName(t *testing.T) {
 	params := url.Values{}
 	params.Set("name", "peng")
 
-	req, err := http.NewRequest("POST", "setName", strings.NewReader(params.Encode()))
+	req, err := http.NewRequest(http.MethodPost, "setName", strings.NewReader(params.Encode()))
 	if err != nil {
 		t.Errorf("new request error %v", err)
 	}
