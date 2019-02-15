01b7445527383833217fe2a8b824e91b19b699b0
lpxxn <mi_duo@live.com>
2019-02-15 15:59:53 +0800

    net dail host and reqeust

---
+++ apihttptest1/readresponse/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type MyConnType struct {
+}
+
+func (mct *MyConnType) SendMsg(urlStr, msg string) (*http.Response, error) {
+	uri, err := url.Parse(urlStr)
+
+	conn, err := net.Dial("tcp", uri.Host+":80")
+	if err != nil {
+		return nil, err
+	}
+
+	reqB := bytes.NewBufferString(msg)
+	defer reqB.Reset()
+
+	req, err := http.NewRequest("GET", urlStr, reqB)
+	if err != nil {
+		return nil, err
+	}
+	if err = req.Write(conn); err != nil {
+		return nil, err
+	}
+	//buff := bufio.NewReader(conn)
+	resp, err := http.ReadResponse(bufio.NewReader(conn), req)
+	return resp, nil
+}
+
+func (mct *MyConnType) SendMsg2(urlStr, msg string) (*http.Response, error) {
+	uri, err := url.Parse(urlStr)
+
+	conn, err := net.Dial("tcp", uri.Host+":80")
+	if err != nil {
+		return nil, err
+	}
+
+	//reqB := bytes.NewBufferString(msg)
+	//defer reqB.Reset()
+	//rc := ioutil.NopCloser(reqB)
+	rc := ioutil.NopCloser(strings.NewReader(msg))
+
+	req := &http.Request{
+		Method: "GET",
+		URL: uri,
+		Body:          rc,
+		ContentLength: int64(len(msg)),
+		Host:          uri.Host,
+	}
+	if err = req.Write(conn); err != nil {
+		return nil, err
+	}
+	//buff := bufio.NewReader(conn)
+	resp, err := http.ReadResponse(bufio.NewReader(conn), req)
+	return resp, nil
+}
+
+func (mct *MyConnType) SendMsgFun(urlStr string, f func(urlStr, msg string) (*http.Response, error)) {
+	resp, err := f(urlStr, "abcde")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	rev := new(RevData)
+	err = json.Unmarshal(body, rev)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("rev data: ", rev)
+}
+
+func main() {
+	urlStr := "http://my-json-server.typicode.com/lpxxn/mygotest/posts/1"
+	mct := new(MyConnType)
+	mct.SendMsgFun(urlStr, mct.SendMsg)
+	fmt.Println("------------")
+	mct.SendMsgFun(urlStr, mct.SendMsg2)
+}
+
+type RevData struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+}
