3b42c13fa43b552947b73786a1bc754cf714fef3
lpxxn <mi_duo@live.com>
2019-03-07 12:03:41 +0800

    test js

--- apihttptest1/mytranslate/main.go
+++ apihttptest1/mytranslate/main.go
@@ -74,8 +74,9 @@ func main() {
 	console.log('------******-----:\n', ioRev);
 	console.log(-805041152 & 4294967295);
 	console.log('end');
+	test = 'abc';
 	`
-
+	// 接收和是最后一个变量值
 	ioRv, err := vm.Run(ioFunc)
 	if err != nil {
 		panic(err)
@@ -83,9 +84,70 @@ func main() {
 
 	fmt.Println("begin js func test")
 
-
-
 	fmt.Println(ioRv, "\n", -805041152 & 4294967295)
+	if r2, err := vm.Run("revgo2 = GO2;"); err != nil {
+		panic(err)
+	} else {
+		fmt.Println("revgo2: ", r2)
+	}
+
+
+	IOFunc := `
+	var Fo = function(a) {
+		return function() {
+			return a
+		}
+	}, Go = function(a, b) {
+		for (var c = 0; c < b.length - 2; c += 3) {
+			var d = b.charAt(c + 2);
+			d = "a" <= d ? d.charCodeAt(0) - 87 : Number(d);
+			d = "+" == b.charAt(c + 1) ? a >>> d : a << d;
+			a = "+" == b.charAt(c) ? a + d & 4294967295 : a ^ d
+		}
+		return a
+	}, Io = function(a, b) {
+		//if (null !== Ho)
+		//	var b = Ho;
+		//else {
+		//	b = Fo(String.fromCharCode(84));
+		//	var c = Fo(String.fromCharCode(75));
+		//	b = [b(), b()];
+		//	b[1] = c();
+		//	b = (Ho = window[b.join(c())] || "") || ""
+		//}
+		var d = Fo(String.fromCharCode(116));
+		c = Fo(String.fromCharCode(107));
+		d = [d(), d()];
+		d[1] = c();
+		c = "&" + d.join("") + "=";
+		d = b.split(".");
+		b = Number(d[0]) || 0;
+		for (var e = [], f = 0, g = 0; g < a.length; g++) {
+			var k = a.charCodeAt(g);
+			128 > k ? e[f++] = k : (2048 > k ? e[f++] = k >> 6 | 192 : (55296 == (k & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (k = 65536 + ((k & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+			e[f++] = k >> 18 | 240,
+			e[f++] = k >> 12 & 63 | 128) : e[f++] = k >> 12 | 224,
+			e[f++] = k >> 6 & 63 | 128),
+			e[f++] = k & 63 | 128)
+		}
+		a = b;
+		for (f = 0; f < e.length; f++)
+			a += e[f],
+			a = Go(a, "+-a^+6");
+		a = Go(a, "+-3^+b+-f");
+		a ^= Number(d[1]) || 0;
+		0 > a && (a = (a & 2147483647) + 2147483648);
+		a %= 1E6;
+		return c + (a.toString() + "." + (a ^ b))
+	};
+	ioRev = Io("summe", "431091.2043743068");
+	console.log(ioRev)
+	`
+	if r2, err := vm.Run(IOFunc); err != nil {
+		panic(err)
+	} else {
+		fmt.Println("revgo2: ", r2)
+	}
 
 	summar := "夏日"
 
