b16af98610520888a351d9acb182178d820cd1b9
lpxxn <mi_duo@live.com>
2019-03-12 14:02:33 +0800

    size of string

--- apihttptest1/bidirection_http/cli/main.go
+++ apihttptest1/bidirection_http/cli/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("hello"))
+		writer.Write([]byte(" world"))
+		writer.Write([]byte("!"))
+
+	})
+	if err := http.ListenAndServe(":9999", nil); err != nil {
+		panic(err)
+	}
 }
--- goreflect/demo6/main.go
+++ goreflect/demo6/main.go
@@ -8,6 +8,14 @@ import (
 	"unsafe"
 )
 
+const (
+	BYTE = 1 << (10 * iota)
+	KILOBYTE
+	MEGABYTE
+	GIGABYTE
+	TERABYTE
+)
+
 func main() {
 	a := struct {}{}
 	size1 := unsafe.Sizeof(a)
@@ -24,10 +32,38 @@ func main() {
 	i := int64(1)
 	fmt.Println("int64 size of :", unsafe.Sizeof(i))
 
+	str2 := ""
+	fmt.Println("string size of :", unsafe.Sizeof(str2))
+
+
 	m1 := make(map[int64]struct{})
 	var m_size1, m_size2 runtime.MemStats
 	fmt.Println("big map size of :", unsafe.Sizeof(m1))
 	runtime.ReadMemStats(&m_size1)
+
+	str1 := "helloworld"
+	fmt.Println("string str1 size", unsafe.Sizeof(str1))
+	fmt.Println("string str1 size", uintptr(len(str1)) * reflect.TypeOf(str1).Size())
+
+	//bytes1 := []byte(str1)
+	bytes1 := make([]byte, len(str1), len(str1))
+	for idx, v := range str1 {
+		bytes1[idx] = byte(v)
+	}
+	fmt.Println("bytes1 size", unsafe.Sizeof(bytes1))
+
+	// right
+	bytes1Size := uintptr(len(bytes1)) * reflect.TypeOf(bytes1).Elem().Size()
+	fmt.Println("bytes1Size size", bytes1Size)
+	// right
+	fmt.Println("binary.Size of bytes1: ", binary.Size(bytes1))
+	// 0.00976563
+	fmt.Println("kb", float64(binary.Size(bytes1))/KILOBYTE)
+
+	// suppose  0.2 second
+	fmt.Println("mb/s", float64(binary.Size(bytes1))/MEGABYTE/0.2)
+
+	fmt.Println("binary.Size of str1: ", binary.Size(str1))
 
 	for ; i < 5000000; i++ {
 		m1[i] = struct{}{}
