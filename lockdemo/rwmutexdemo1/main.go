b2b2c4db156c3843c99454dd4f7dff836e50372a
lpxxn <mi_duo@live.com>
2019-03-15 11:27:47 +0800

    test mux

--- lockdemo/rwmutexdemo1/main.go
+++ lockdemo/rwmutexdemo1/main.go
@@ -9,24 +9,20 @@ import (
 )
 
 type M1 struct {
-	mux sync.Mutex
-	v int
+	rw sync.RWMutex
 }
 
-// must be pointer because all parameters are passed by value
 func (m *M1) Func1() {
-	m.mux.Lock()
-	defer m.mux.Unlock()
+	m.rw.Lock()
+	defer m.rw.Unlock()
 	fmt.Println("func1 running")
-	m.v++
 	time.Sleep(time.Second * 3)
 	fmt.Println("111 end")
 }
 
 func (m *M1) Func2() {
-	m.mux.Lock()
-	defer m.mux.Unlock()
-	m.v++
+	m.rw.RLock()
+	defer m.rw.RUnlock()
 	fmt.Println("2222 running")
 	time.Sleep(time.Second * 3)
 	fmt.Println("222 end")
@@ -37,6 +33,10 @@ func main() {
 
 	go m1.Func2()
 	go m1.Func1()
+
+	go m1.Func1()
+
+
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, os.Interrupt)
