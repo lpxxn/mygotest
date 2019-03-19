f58a441cbe2caff970604f53e128a6c7b0df90f5
lpxxn <mi_duo@live.com>
2019-03-19 11:22:13 +0800

    parse form

--- apihttptest1/bidirection_http/srv/main.go
+++ apihttptest1/bidirection_http/srv/main.go
@@ -1,1 +1,18 @@
-package srv
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	bodyStr := `{"a" = "aaaa"}`
+	re, err := http.NewRequest(http.MethodPost, "http://www.baidu.com", strings.NewReader(bodyStr))
+	re.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(re.ParseForm())
+	fmt.Println(re.Form)
+}
