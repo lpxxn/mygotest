6664b25f0abd1ea6b76ec38ccbf8e627c0be9cfa
lpxxn <mi_duo@live.com>
2019-03-27 13:59:22 +0800

    unmarshal data

---
+++ jsoniterdemo/demo4/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const revData = `
+{
+    "resultCode": "OK",
+    "resultDescription": "SUCCESS",
+    "data": {
+        "tradeNo": 114993140347196113,
+        "next": {
+            "id": 114993140347196113,
+            "createTime": 1551770052,
+            "amountInCent": 3,
+            "status": "New",
+            "orderId": "MEICAN_MAIN-Recharge-155211278720180227",
+            "clientId": 237879989763099715,
+            "paymentServiceId": 223381299346328643,
+            "effectiveAmountInCent": 0,
+            "effectiveTime": 0,
+            "finishTime": 0,
+            "subject": "在线支付",
+            "description": "在线支付",
+            "userIdentifier": "2dd22fedb832"
+        }
+    }
+}
+`
+
+type A struct {
+	ResultCode string `json:"resultCode"`
+	Data struct{
+		TradeNo int64 `json:"tradeNo"`
+	} `json:"data"`
+}
+
+func main()  {
+	mapData := make(map[string]interface{})
+	if err := json.Unmarshal([]byte(revData), &mapData); err != nil {
+		panic(err)
+	}
+	a  := A{}
+	if err := json.Unmarshal([]byte(revData), &a); err != nil {
+		panic(err)
+	}
+	fmt.Printf("%#v\n", mapData)
+	revStr, err := json.Marshal(mapData)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(revStr))
+}
