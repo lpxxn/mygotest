ef19b8e060a181b4ba4b6efcff675405d347a0f2
lpxxn <mi_duo@live.com>
2019-04-08 16:09:39 +0800

    remove data from slice

---
+++ slicetest/remove/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type aRRR []int
+
+func (ar *aRRR) RemoveEle(v int) {
+	pA := *ar
+	lenAr := len(pA)
+	for i := lenAr - 1; i >= 0; i-- {
+		if pA[i] > v {
+			pA = append(pA[:i], pA[i+1:]...)
+		}
+	}
+	*ar = pA
+}
+
+func (ar aRRR) RemoveEle2(v int) {
+	for i := len(ar) - 1; i >= 0; i-- {
+		if ar[i] > v {
+			ar = append(ar[:i], ar[i+1:]...)
+		}
+	}
+}
+
+func main() {
+	ar := aRRR{1, 10, 2, 3, 8, 4, 2, 15}
+	ar.RemoveEle(5)
+	fmt.Println(ar)
+
+	ar2 := aRRR{1, 10, 2, 3, 8, 4, 2, 15}
+	ar2.RemoveEle(5)
+	fmt.Println(ar2)
+}
