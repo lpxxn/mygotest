b9fdadf9510385d0542cba21acb2f305878225c4
lpxxn <mi_duo@live.com>
2019-04-11 15:25:11 +0800

    millsecondtime

---
+++ timetest/java_milltimestemp/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func makeTimestamp(t time.Time) int64 {
+	return t.UnixNano() / int64(time.Millisecond)
+}
+
+func main() {
+	t := time.Now()
+	javaTime := makeTimestamp(t)
+	fmt.Println("currentTime : ", t)
+	fmt.Println("javaTime    : ", javaTime)
+	pT := time.Unix(0, javaTime*int64(time.Millisecond))
+	fmt.Println("pt          : ", pT)
+
+
+    //return pData[i].ParseCreateTime().After(pData[j].ParseCreateTime())
+}
