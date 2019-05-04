e07600920478642ee307e5de95f72cf70e50592b
lpxxn <mi_duo@live.com>
2019-05-04 11:54:15 +0800

    bidrection http

--- httpdemo1/httptest3/main.go
+++ httpdemo1/httptest3/main.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"net"
 	"net/http"
-	"net/url"
-	"strings"
-	"time"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.String())
-	fmt.Println(r.URL.RawQuery)
-	fmt.Println(r.Host)
-	//fmt.Fprintln(w, "hello world")
-	w.Write([]byte("hello "))
-	w.Write([]byte("world !"))
-	w.Write([]byte(io.EOF.Error()))
+	fmt.Fprintln(w, "hello world ! ")
 }
 
-func bidirectionFunc(w http.ResponseWriter, r *http.Request) {
+func cbHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	btys, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -30,57 +18,13 @@ func bidirectionFunc(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("read body err" + err.Error()))
 		return
 	}
-	fmt.Println("received msg: ", string(btys))
-	w.Write(append([]byte("i received "), btys...))
+	fmt.Fprintln(w, "received msg: ", string(btys))
 }
 
 func main() {
 	http.HandleFunc("/", IndexHandler)
-	http.HandleFunc("/cb", IndexHandler)
-	http.HandleFunc("/bd", bidirectionFunc)
-	time.AfterFunc(time.Second, CbRequest)
+	http.HandleFunc("/cb", cbHandler)
 	if err := http.ListenAndServe(":9100", nil); err != nil {
 		panic(err)
 	}
-}
-
-var testUrl = "http://127.0.0.1:9100/bd?name=aaaaaaa"
-func CbRequest() {
-	uri, _ := url.Parse(testUrl)
-	body1 := `{"name": "li"}`
-	r, _ := http.NewRequest(http.MethodPost, uri.String(), strings.NewReader(body1))
-	_ = net.JoinHostPort(uri.Host, uri.Port())
-	conn, err := net.Dial("tcp", "127.0.0.1:9100")
-	if err != nil {
-		panic(err)
-	}
-	if err := r.Write(conn); err != nil {
-		fmt.Println(err)
-		return
-	}
-	b2 := `{"name": "peng"}`
-	r.ContentLength = int64(len(b2))
-	r.Body = ioutil.NopCloser(strings.NewReader(b2))
-	if err := r.Write(conn); err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("begin read:============")
-	resp, err := http.ReadResponse(bufio.NewReader(conn), r)
-	defer resp.Body.Close()
-	rspBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	println(string(rspBody))
-	resp2, err := http.ReadResponse(bufio.NewReader(conn), r)
-	defer resp2.Body.Close()
-	rspBody2, err := ioutil.ReadAll(resp2.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	println(string(rspBody2))
-	//http.DefaultClient.Do(r)
 }
