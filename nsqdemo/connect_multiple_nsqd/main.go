95042257d932a4c80cd54b2f0acf285c2d346381
lpxxn <mi_duo@live.com>
2019-08-30 15:41:54 +0800

    nsq test

---
+++ nsqdemo/connect_multiple_nsqd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/nsqio/go-nsq"
+)
+
+func main() {
+	adds := []string{"127.0.0.1:7000", "127.0.0.1:8000"}
+	config := nsq.NewConfig()
+	config.MaxInFlight = 1000
+	config.MaxBackoffDuration = 5 * time.Second
+	config.DialTimeout = 10 * time.Second
+
+	topicName := "testTopic1"
+	c, _ := nsq.NewConsumer(topicName, "ch1", config)
+	testHandler := &MyTestHandler{consumer: c}
+
+	c.AddHandler(testHandler)
+	if err := c.ConnectToNSQDs(adds); err != nil {
+		panic(err)
+	}
+	stats := c.Stats()
+	if stats.Connections == 0 {
+		panic("stats report 0 connections (should be > 0)")
+	}
+	stop := make(chan os.Signal)
+	signal.Notify(stop, os.Interrupt)
+	fmt.Println("server is running....")
+	<-stop
+}
+
+type MyTestHandler struct {
+	consumer *nsq.Consumer
+}
+
+func (m MyTestHandler) HandleMessage(message *nsq.Message) error {
+	fmt.Println(string(message.Body))
+	return nil
+}
