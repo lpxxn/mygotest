677fc313ec68368471dd8b93242ead02168a7de4
lpxxn <mi_duo@live.com>
2019-11-14 17:10:53 +0800

    fix

--- structquestion/underlinestruct/main_test.go
+++ structquestion/underlinestruct/main_test.go
@@ -12,19 +12,19 @@ type A struct {
 type AList []*A
 
 func TestStructArray(t *testing.T) {
+	// "[]"
 	a1 := AList{}
 	fmt.Printf("%#v \n", a1)
 	b1, _ := json.Marshal(a1)
 	fmt.Printf("%#v \n", string(b1))
 
-
+	// "[]"
 	a2 := make(AList, 0, 1)
 	fmt.Printf("%#v \n", a2)
 	b2, _ := json.Marshal(a2)
 	fmt.Printf("%#v \n", string(b2))
 
-
-	// nil
+	// "null"
 	var a3 AList
 	fmt.Printf("%#v \n", a3)
 	b3, _ := json.Marshal(a3)
