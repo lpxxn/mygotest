14e10d37c3ba53b51f1b1fb34b23da7f5b3bea8e
lpxxn <mi_duo@live.com>
2019-11-29 17:06:55 +0800

    feat: tty

--- moniterrun/main.go
+++ moniterrun/main.go
@@ -1,68 +1,76 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
 	"os/signal"
 	"sync"
+	"time"
 
 	"github.com/creack/pty"
 )
 
-var exitCh = make(chan os.Signal)
-
 func main() {
-	cmd := exec.Command("./webserverdemo1", "-env=local -a=bcd", "-abc=ddd")
-	//cmd := exec.Command("./basi", "-env=local")
-	cmd.Env = append(cmd.Env, "ENV=dev")
-	//cmd.Stdout = os.Stdout
-	fmt.Println(cmd.String())
-	fmt.Println(cmd.Env)
-	//cmdReader, err := cmd.StdoutPipe()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//scanner := bufio.NewScanner(cmdReader)
-	//go func() {
-	//	for scanner.Scan() {
-	//		fmt.Println(scanner.Text())
-	//		//fmt.Println(scanner.Text())
-	//	}
-	//}()
-	//
-	//if err := cmd.Start(); err != nil {
-	//	panic(err)
-	//}
-	//
-	//if err := cmd.Wait(); err != nil {
-	//	panic(err)
-	//}
-	f, err := pty.Start(cmd)
-	if err != nil {
-		panic(err)
-	}
-	io.Copy(os.Stdout, f)
+	var exitCh = make(chan os.Signal)
+	ctx, cancel := context.WithCancel(context.Background())
 
 	signal.Notify(exitCh, os.Interrupt)
+
+	go RunServer(ctx)
+
 	fmt.Println("run service")
 	<-exitCh
+	cancel()
+	time.Sleep(time.Second)
 	fmt.Println("exit service")
 }
 
-func RunServer() {
+func RunServer(ctx context.Context) {
 	wg := sync.WaitGroup{}
+
 	wg.Add(1)
 	go func() {
-		cmd := exec.Command("webserverdemo1")
+		cmd := exec.Command("./webserverdemo1", "-env=local -a=bcd", "-abc=ddd")
+		//cmd := exec.Command("./basi", "-env=local")
 		cmd.Env = append(cmd.Env, "ENV=dev")
-		cmd.Run()
+		//cmd.Stdout = os.Stdout
+		fmt.Println(cmd.String())
+		fmt.Println(cmd.Env)
+		//cmdReader, err := cmd.StdoutPipe()
+		//if err != nil {
+		//	panic(err)
+		//}
+		//scanner := bufio.NewScanner(cmdReader)
+		//go func() {
+		//	for scanner.Scan() {
+		//		fmt.Println(scanner.Text())
+		//		//fmt.Println(scanner.Text())
+		//	}
+		//}()
+		//
+		//if err := cmd.Start(); err != nil {
+		//	panic(err)
+		//}
+		//
+		//if err := cmd.Wait(); err != nil {
+		//	panic(err)
+		//}
+		f, err := pty.Start(cmd)
+		if err != nil {
+			panic(err)
+		}
+		io.Copy(os.Stdout, f)
 	}()
 	go func() {
 
 		select {
-		case <-exitCh:
+		case <-ctx.Done():
 		}
+		fmt.Println("done")
+		wg.Done()
 	}()
+	wg.Wait()
 }
