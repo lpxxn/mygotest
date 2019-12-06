af8665f488a676f082582dc0b5652fb8fc8a0deb
lpxxn <mi_duo@live.com>
2019-12-06 14:24:29 +0800

    feat: compare struct

--- structquestion/underlinestruct/main_test.go
+++ structquestion/underlinestruct/main_test.go
@@ -31,3 +31,62 @@ func TestStructArray(t *testing.T) {
 	fmt.Printf("%#v \n", string(b3))
 
 }
+
+// Color QRCode color
+type Color struct {
+	R string `json:"r"`
+	G string `json:"g"`
+	B string `json:"b"`
+}
+
+// UnlimitedQRCode
+type UnlimitedQRCode struct {
+	// 最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：
+	Scene string `json:"scene"`
+	Page  string `json:"page,omitempty"`
+	// 二维码的宽度，单位 px，最小 280px，最大 1280px
+	Width     int  `json:"width,omitempty"`
+	AutoColor bool `json:"auto_color,omitempty"`
+	// auto_color 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"} 十进制表示
+	LineColor Color `json:"line_color,omitempty"`
+	// 是否需要透明底色，为 true 时，生成透明底色的小程序
+	IsHyaline bool `json:"is_hyaline,omitempty"`
+}
+
+func TestCompareStruct(t *testing.T) {
+	s1 := &UnlimitedQRCode{
+		Scene:     "",
+		Page:      "",
+		Width:     0,
+		AutoColor: false,
+		LineColor: Color{},
+		IsHyaline: false,
+	}
+	s2 := &UnlimitedQRCode{
+		Scene:     "",
+		Page:      "",
+		Width:     0,
+		AutoColor: false,
+		LineColor: Color{},
+		IsHyaline: false,
+	}
+
+	if s1 == s2 {
+		t.Fatal("pointer equal error")
+	}
+
+	if *s1 != *s2 {
+		t.Fatal("not equal")
+	}
+	s1.LineColor.B = "aa"
+
+	if *s1 == *s2 {
+		t.Fatal("not equal")
+	}
+	s2.LineColor.B = "aa"
+	s1.Scene = "scene=2002?cid=1&sid=1"
+	s2.Scene = "scene=2002?cid=1&sid=1"
+	if *s1 != *s2 {
+		t.Fatal("not equal")
+	}
+}
