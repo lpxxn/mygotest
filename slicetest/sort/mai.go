3f0139b770b8f8177f80a954c90e3f623aa18ed2
lpxxn <mi_duo@live.com>
2020-01-15 12:01:32 +0800

    time asc

--- slicetest/sort/mai.go
+++ slicetest/sort/mai.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"sort"
 	"fmt"
+	"sort"
+	"time"
 )
 
 type AsortObj struct {
@@ -10,7 +11,6 @@ type AsortObj struct {
 }
 
 type AsortObjArray []AsortObj
-
 
 func (a *AsortObjArray) SortDesc() {
 	pData := *a
@@ -28,10 +28,42 @@ func (a *AsortObjArray) LimitEle(limit int) {
 	*a = pData
 }
 
+type SAbcDT struct {
+	Name     string
+	CreateAt time.Time
+}
+
 func main() {
 	arr := AsortObjArray{AsortObj{5}, AsortObj{1}, AsortObj{18}, AsortObj{10}, AsortObj{8}, AsortObj{89}, AsortObj{6}}
 	arr.SortDesc()
 	fmt.Println(arr)
 	arr.LimitEle(5)
 	fmt.Println(arr)
+
+	var sabcDTArray []SAbcDT
+	sabcDTArray = append(sabcDTArray, SAbcDT{
+		Name:     "cde",
+		CreateAt: time.Now(),
+	})
+
+	sabcDTArray = append(sabcDTArray, SAbcDT{
+		Name:     "abcde",
+		CreateAt: time.Now().AddDate(-1, 0, 0),
+	})
+
+	sabcDTArray = append(sabcDTArray, SAbcDT{
+		Name:     "d",
+		CreateAt: time.Now().AddDate(0, 2, 1),
+	})
+
+	sabcDTArray = append(sabcDTArray, SAbcDT{
+		Name:     "d",
+		CreateAt: time.Now().AddDate(0, -5, 9),
+	})
+
+	fmt.Println(sabcDTArray)
+	sort.Slice(sabcDTArray, func(i, j int) bool {
+		return sabcDTArray[i].CreateAt.Before(sabcDTArray[j].CreateAt)
+	})
+	fmt.Println("asc: ", sabcDTArray)
 }
