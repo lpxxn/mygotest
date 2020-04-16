78b923115fe2018e47405671b4065f827a7551b2
lpxxn <mi_duo@live.com>
2020-04-16 11:42:54 +0800

    elpate

--- charaterEncoder/demo1/main.go
+++ charaterEncoder/demo1/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"time"
 
 	"golang.org/x/text/encoding/simplifiedchinese"
 )
@@ -14,6 +16,21 @@ func main() {
 	encoderStr("0.5")
 	encoderStr("<食堂菜单>2018-07-19~2018-07-31[按天重复_11]") // 44大于 40
 	encoderStr("2019-03-17 起[按天重复_27]")
+
+	const dateTimeFormat = "060102150405"
+	updateTime := time.Now()
+	buf := new(bytes.Buffer)
+
+	// 餐品的最后修改时间, 格式 YYMMDDhhmmss, ASCII 32 bytes, 空位补 0.
+	timeStr := updateTime.Format(dateTimeFormat)
+	timeBytes := make([]byte, 0, 32)
+	timeBytes = append(timeBytes, []byte(timeStr)...)
+	fmt.Println(timeBytes, len(timeBytes))
+	// 补足 32 bytes.
+	timeBytes = append(timeBytes, make([]byte, 32-len(timeBytes))...)
+	fmt.Println(timeBytes, len(timeBytes))
+	fmt.Println(string(timeBytes), len(timeBytes))
+	buf.Write(timeBytes)
 }
 
 func encoderStr(s string) {
