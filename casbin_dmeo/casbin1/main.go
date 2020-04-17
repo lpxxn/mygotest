169d6dec117044210ff171c3db287a8da37e02d4
lpxxn <mi_duo@live.com>
2020-04-17 11:55:22 +0800

    casbin demo

--- casbin_dmeo/casbin1/main.go
+++ casbin_dmeo/casbin1/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/casbin/casbin"
+)
+
+func main() {
+	e, err := casbin.NewEnforcer("rbac_with_domains_model.conf", "rbac_with_domains_policy.csv")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(e)
 }
