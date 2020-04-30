2d0cceaf3c82db7fae2a0163b6696800f038ebe4
lpxxn <mi_duo@live.com>
2020-04-30 10:59:26 +0800

    tempalte

--- template_demo/demo3/demo3_test.go
+++ template_demo/demo3/demo3_test.go
@@ -42,3 +42,42 @@ func TestFunc1(t *testing.T) {
 		panic(err)
 	}
 }
+
+// pipeline
+func TestPipline(t *testing.T) {
+	// html 内置函数
+	// 内置函数 https://golang.org/pkg/text/template/#hdr-Functions
+	// 左边的输出作为右边的输入
+
+	tmpl, err := template.New("test").Parse(`{{. | html}} {{. | urlquery}}
+	{{with $x := "output"}}{{$x}}{{end}}
+    {{with $x := "output"}}{{$x | printf "abc: %s"}}{{end}}
+	`)
+
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, map[string]interface{}{"name": "中国"})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestBuildInFunctionCall(t *testing.T) {
+	type X struct {
+		Y func(a int, b int) int
+	}
+	x := X{Y: func(a int, b int) int {
+		return a + b
+	}}
+	tmpl, err := template.New("test").Parse(`{{call .X.Y 1 2}}
+	`)
+
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, map[string]interface{}{"X": x})
+	if err != nil {
+		panic(err)
+	}
+}
