0cb08bed62f6fa37c22a9aa5d97e0a4023e49542
lpxxn <mi_duo@live.com>
2020-05-21 22:19:32 +0800

    run cmd

---
+++ exec_demo/bad_test/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	a := 0
+	_ = a
+}
+// /Users/lipeng/go/src/github.com/mygotest/exec_demo/bad_test
+
+// GOOS=linux go build /Users/lipeng/go/src/github.com/mygotest/exec_demo/bad_test
