0f716532454817e7c6476d8e2b77b048a1f730e4
lpxxn <mi_duo@live.com>
2020-06-11 22:56:34 +0800

    token

---
+++ token_ast_demo/demo1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"go/scanner"
+	"go/token"
+)
+
+func main() {
+	src := []byte(`
+var a bool = true
+b := 1 + 2.0
+println("hello")
+fmt.Println("world")
+`)
+	fset := token.NewFileSet()
+	file := fset.AddFile("hello.go", fset.Base(), len(src))
+	var s scanner.Scanner
+	s.Init(file, src, nil, scanner.ScanComments)
+	for {
+		pos, tok, lit := s.Scan()
+		if tok == token.EOF {
+			break
+		}
+		fmt.Printf("info pos:%s\t token:%s\t lit:%q\n", fset.Position(pos), tok, lit)
+	}
+
+}
