c0eea6b6c4228c233c9b582db1f01d2a0302ca05
lpxxn <mi_duo@live.com>
2020-07-07 14:03:43 +0800

    fix bug

--- token_ast_demo/ast_import_test.go
+++ token_ast_demo/ast_import_test.go
@@ -28,7 +28,6 @@ func TestImport(t *testing.T) {
 Go语⾔中通过type关键字声明类型：⼀种是声明新的类型，另⼀种是为已
 有的类型创建⼀个别名
 */
-
 func TestAlia(t *testing.T) {
 	// 新类型
 	type MyInt1 int
@@ -63,11 +62,36 @@ func TestAlia(t *testing.T) {
 						t.Logf("typsSpec name: %#v type: %#v asign: %#v \n", ts.Name, ts.Type, ts.Assign)
 					}
 					/*
-					其中最重要的是 TypeSpec.Name 成员，表示新声明类型的名字或者是已
-					有类型的别名。⽽ TypeSpec.Assign 成员对应 = 符号的位置，如果该
-					成员表示的位置有效，则表示这是为已有类型定义⼀个别名（⽽不是定义
-					新的类型）
-					 */
+						其中最重要的是 TypeSpec.Name 成员，表示新声明类型的名字或者是已
+						有类型的别名。⽽ TypeSpec.Assign 成员对应 = 符号的位置，如果该
+						成员表示的位置有效，则表示这是为已有类型定义⼀个别名（⽽不是定义
+						新的类型）
+					*/
+				}
+			}
+		}
+	}
+}
+
+func TestConst(t *testing.T) {
+	src := `package foo
+const Pi = 3.14
+const E float64 = 2.71828
+const a1, a2 = 1, "aaa"
+`
+	fst := token.NewFileSet()
+	f, err := parser.ParseFile(fst, "a.go", src, parser.AllErrors)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, decl := range f.Decls {
+		if v, ok := decl.(*ast.GenDecl); ok {
+			for _, spec := range v.Specs {
+				t.Logf("spec : %#v\n", spec)
+				if vs, ok := spec.(*ast.ValueSpec); ok {
+					t.Logf("ValueSpec name: %#v type: %#v values: %#v \n", vs.Names, vs.Type, vs.Values)
+					ast.Print(nil, vs)
+
 				}
 			}
 		}
