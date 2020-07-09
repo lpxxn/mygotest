3ae1cd8e10fc13407612cc6fd62f8453f385cc90
lpxxn <mi_duo@live.com>
2020-07-09 17:14:08 +0800

    ssh

--- exec_demo/run_cmd_test.go
+++ exec_demo/run_cmd_test.go
@@ -4,7 +4,19 @@ import "testing"
 
 func TestRemoteCmd(t *testing.T) {
 	// /home/ec2-user/baseinfo
-	err := RunSSHCmd("cafe1sandboxdev", "cd /home/ec2-user/baseinfo && ls; pwd")
+	// 会话[:窗口:面板]
+	// work:0  work:1  work:baseinfo
+	err := RunSSHCmd("cafe2sandboxdev", "cd /home/ec2-user/baseinfo && ls; pwd&& tmux capture-pane -t work:2 && tmux show-buffer")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRemoteCmd2(t *testing.T) {
+	// /home/ec2-user/baseinfo
+	// 会话[:窗口:面板]
+	// work:0  work:1  work:baseinfo
+	err := RunSSHCmd("cafe2sandboxdev", `tmux send-keys -t work:1 "aaaaaaa" C-m`)
 	if err != nil {
 		t.Fatal(err)
 	}
