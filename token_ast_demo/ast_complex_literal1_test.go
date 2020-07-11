3cebc1c2b63590096b6c5c5ad57e776bca89be2a
lpxxn <mi_duo@live.com>
2020-07-11 11:38:10 +0800

    ssh

--- token_ast_demo/ast_complex_literal1_test.go
+++ token_ast_demo/ast_complex_literal1_test.go
@@ -6,7 +6,9 @@ import (
 	"testing"
 )
 
+// 函数面值
 func TestFuncLiteral1(t *testing.T) {
+	// FunctionLit   = "func" Signature FunctionBody .
 	// 同样是由func关键字开始，后面是函数签名（输入参数和返回值）和函数体。函数面值和函数声明的最大差别是没有函数名字。
 	// 该函数面值没有输入参数和返回值，同时函数体也没有任何语句，而且没有涉及上下文的变量引用，可以说是最简单的函数面值。因为面值也是一种表达式，因此可以用表达式的方式解析其语法树：
 	expr, err := parser.ParseExpr("func() {}")
