5ad5deda0724ac49f3d7589c1f345154e98f2fe5
lpxxn <mi_duo@live.com>
2020-07-14 11:54:06 +0800

    ast

--- token_ast_demo/ast_block_test.go
+++ token_ast_demo/ast_block_test.go
@@ -6,6 +6,7 @@ import (
 	"go/token"
 	"testing"
 )
+
 // stmt expr
 // 是 statement 和expression 也就是 语句 和 表达式
 // 10 语句块和语句
@@ -40,4 +41,61 @@ func main() {
 	}
 	//ast.Print(nil, f)
 	ast.Print(nil, f.Decls[0].(*ast.FuncDecl))
+}
+
+func TestBlockFunc3(t *testing.T) {
+	src := `package pkg_a
+func main() {
+	123
+}
+`
+	fst := token.NewFileSet()
+	f, err := parser.ParseFile(fst, "a.go", src, parser.AllErrors)
+	if err != nil {
+		t.Fatal(err)
+	}
+	//ast.Print(nil, f)
+	ast.Print(nil, f.Decls[0].(*ast.FuncDecl))
+
+	src = `package pkg_a
+func main() {
+	{
+		123
+	}
+}
+`
+	f, err = parser.ParseFile(fst, "a.go", src, parser.AllErrors)
+	if err != nil {
+		t.Fatal(err)
+	}
+	//ast.Print(nil, f)
+	ast.Print(nil, f.Decls[0].(*ast.FuncDecl))
+}
+
+func TestBlockFunc4(t *testing.T) {
+	src := `package pkg_a
+func main() {
+	a := 123
+	var b string = "abc"
+	a = 2
+}
+`
+	fst := token.NewFileSet()
+	f, err := parser.ParseFile(fst, "a.go", src, parser.AllErrors)
+	if err != nil {
+		t.Fatal(err)
+	}
+	//ast.Print(nil, f)
+	ast.Print(nil, f.Decls[0].(*ast.FuncDecl))
+	/*
+			type DeclStmt struct {
+			    Decl Decl // *GenDecl with CONST, TYPE, or VAR token
+			}
+		type AssignStmt struct {
+		    Lhs    []Expr
+		    TokPos token.Pos   // position of Tok
+		    Tok    token.Token // assignment token, DEFINE  是 = 或者 :=
+		    Rhs    []Expr
+		}
+	*/
 }
