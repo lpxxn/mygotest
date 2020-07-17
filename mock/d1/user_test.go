840f2ab120a293d3dfffc50ea35670ddfe59fd9f
lpxxn <mi_duo@live.com>
2020-07-17 17:13:18 +0800

    mock

---
+++ mock/d1/user_test.go
@@ -0,0 +1,54 @@
+package d1
+
+import (
+	"testing"
+
+	"github.com/caicloud/nirvana/log"
+	"github.com/golang/mock/gomock"
+	mock_data "github.com/mygotest/mock/d1/dao"
+)
+
+func TestUser1(t *testing.T) {
+	mockUser := mock_data.NewMockUser(gomock.NewController(t))
+	mockUser.EXPECT().V(1, "2").Do(func(id int, name string) {
+		t.Log(id, " ", name)
+	}).Do(func(id int, name string) {
+		t.Log("do2 ", id)
+	}).Return("a", nil)
+	// error 要把所有的参数都加上才可以，也不能多值
+	//Do(func(name string) {
+	//	t.Log("do2 ", name)
+	//})
+	//mockUser.EXPECT().V(1, "2").Return("a", nil)
+	var u User = mockUser
+	a, err := u.V(1, "2")
+	t.Log(a, err)
+}
+
+func TestUser2(t *testing.T) {
+	mockUser := mock_data.NewMockUser(gomock.NewController(t))
+	mockUser.EXPECT().V(1, "2")
+	// 没有 return 会返回空
+	//mockUser.EXPECT().V(1, "2").Return("a", nil)
+	var u User = mockUser
+	a, err := u.V(1, "2")
+	t.Log(a, err)
+}
+
+func TestUser3(t *testing.T) {
+	mockUser := mock_data.NewMockUser(gomock.NewController(t))
+	mockUser.EXPECT().V(1, "2").DoAndReturn(func(idx int, n string) (string, error) {
+		log.Info(idx, "  ", n)
+		return "1", nil
+	}).DoAndReturn(func(idx int, n string) (string, error) {
+		log.Info(idx, "  ", n)
+		return "2", nil
+	})
+	// 没有有多个 return 会返回最后一个
+	var u User = mockUser
+	a, err := u.V(1, "2")
+	t.Log(a, err)
+	mockUser.EXPECT().V(1, "2").Return("c", nil).Return("d", nil)
+	a, err = u.V(1, "2")
+	t.Log(a, err)
+}
