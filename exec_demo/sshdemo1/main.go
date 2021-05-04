6d23e1169c14bdb5105027e2bac0e6707a41ecc0
lpxxn <mi_duo@live.com>
2021-05-04 16:36:39 +0800

    feat: test

---
+++ exec_demo/sshdemo1/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	var hostKey ssh.PublicKey
+
+	// Create client config
+	config := &ssh.ClientConfig{
+		User: "li",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("lipeng1019"),
+		},
+		HostKeyCallback: ssh.FixedHostKey(hostKey),
+	}
+	// Connect to ssh server
+
+	conn, err := ssh.Dial("tcp", "127.0.0.1:22", config)
+	if err != nil {
+		log.Fatal("unable to connect: ", err)
+	}
+	defer conn.Close()
+	// Create a session
+	session, err := conn.NewSession()
+	if err != nil {
+		log.Fatal("unable to create session: ", err)
+	}
+	defer session.Close()
+	// Set up terminal modes
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+	// Request pseudo terminal
+	if err := session.RequestPty("xterm", 40, 80, modes); err != nil {
+		log.Fatal("request for pseudo terminal failed: ", err)
+	}
+	// Start remote shell
+	if err := session.Shell(); err != nil {
+		log.Fatal("failed to start shell: ", err)
+	}
+}
---
+++ exec_demo/sshdemo2/main.go
@@ -0,0 +1,1 @@
+package sshdemo2
