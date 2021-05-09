ecddfdb2ecb604191416ff3adbc0fc97189e28c7
lpxxn <mi_duo@live.com>
2021-05-09 18:48:53 +0800

    feat: test

--- exec_demo/exec_demo1/main.go
+++ exec_demo/exec_demo1/main.go
@@ -32,4 +32,24 @@ end tell
 '
 `)
 	fmt.Printf("err: %#v\n", err)
+
+
 }
+/*
+
+tell application "iTerm"
+	activate
+
+	set W to current window
+	if W = missing value then set W to create window with default profile
+	tell W's current session
+		split vertically with default profile
+		split vertically with default profile
+	end tell
+	set T to W's current tab
+	write T's session 1 text "cd ~/Desktop"
+	write T's session 2 text "cd ~/Downloads"
+	write T's session 3 text "cd ~/Documents"
+end tell
+
+ */
