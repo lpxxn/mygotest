26b60f79c707dbb8d91d85882ffba03b776de11e
lpxxn <mi_duo@live.com>
2021-10-27 17:31:38 +0800

    test redis failover

--- goredis/r_test.go
+++ goredis/r_test.go
@@ -1,5 +1,79 @@
-package main
+package goredis
 
-func main() {
-	$END$
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+func TestRedisCluster(t *testing.T) {
+	// cafeteria-c-test1.lbqctm.clustercfg.cnw1.cache.amazonaws.com.cn:6379
+	client := redis.NewClusterClient(&redis.ClusterOptions{Addrs: []string{"cafeteria-c-test1.lbqctm.clustercfg.cnw1.cache.amazonaws.com.cn:6379"}})
+	key1 := "a"
+	for {
+		time.Sleep(time.Second * 2)
+		cmd := client.Set(context.Background(), key1, "a", -1)
+		if cmd.Err() != nil {
+			t.Log(cmd.Err())
+			continue
+		}
+		t.Log(cmd.Val())
+		strCmd := client.Get(context.Background(), key1)
+		if strCmd.Err() != nil {
+			t.Log(strCmd.Err())
+			continue
+		}
+		t.Log(strCmd.Val())
+
+	}
+}
+
+// aws master slave方式
+func TestRedisGo(t *testing.T) {
+	//read cafeteria-test1-ro.lbqctm.ng.0001.cnw1.cache.amazonaws.com.cn:6379
+	//Primary  cafeteria-test1.lbqctm.ng.0001.cnw1.cache.amazonaws.com.cn:6379
+	opt := redis.ClusterOptions{
+		DialTimeout:  10 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+
+		MaxRedirects: 8,
+
+		PoolSize:           10,
+		PoolTimeout:        30 * time.Second,
+		IdleTimeout:        time.Minute,
+		IdleCheckFrequency: 100 * time.Millisecond,
+	}
+	opt.ClusterSlots = func(ctx context.Context) ([]redis.ClusterSlot, error) {
+		slots := []redis.ClusterSlot{{
+			Start: 0,
+			End:   16383,
+			Nodes: []redis.ClusterNode{{
+				Addr: "cafeteria-test1.lbqctm.ng.0001.cnw1.cache.amazonaws.com.cn:6379",
+			}, {
+				Addr: "cafeteria-test1-ro.lbqctm.ng.0001.cnw1.cache.amazonaws.com.cn:6379",
+			}},
+		}}
+		return slots, nil
+	}
+	client := redis.NewClusterClient(&opt)
+	key1 := "a"
+	for {
+		time.Sleep(time.Second * 2)
+		cmd := client.Set(context.Background(), key1, "a", -1)
+		if cmd.Err() != nil {
+			t.Log(cmd.Err())
+			continue
+		}
+		t.Log(cmd.Val())
+		strCmd := client.Get(context.Background(), key1)
+		if strCmd.Err() != nil {
+			t.Log(strCmd.Err())
+			continue
+		}
+		t.Log(strCmd.Val())
+
+	}
 }
