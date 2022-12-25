fe40561fc7a360508adf1dc1a95b56597d76078d
lpxxn <mi_duo@live.com>
2022-12-25 22:23:50 +0800

    fix: json

---
+++ jsoniterdemo/demo5/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	j "github.com/tidwall/gjson"
+	sj "github.com/tidwall/sjson"
+)
+
+//var OriginJson =
+
+func main() {
+	myJson := MyJson{}
+	myJson.OriginJson = `{"name":"tom","age":18,"hobby":["basketball","football"],"address":{"city":"beijing","street":"xizhimen"}}`
+	fmt.Println(myJson.OriginJson)
+
+	fmt.Println("------开始遍历--------")
+
+	newVal := "new"
+	myJson.ParseJSON(newVal)
+	//fmt.Println(OriginJson)
+}
+
+type MyJson struct {
+	OriginJson string
+	rev        []string
+}
+
+func (m *MyJson) ParseJSON(newVal string) {
+	m.rev = make([]string, 0)
+	keyPath := ""
+	j.Parse(m.OriginJson).ForEach(func(key, value j.Result) bool {
+		keyPath = key.String()
+		m.parseJSON(key, value, keyPath, newVal)
+		return true
+	})
+}
+
+func (m *MyJson) GetResult() []string {
+	return m.rev
+}
+
+func (m *MyJson) parseJSON(key, value j.Result, keyPath string, newVal string) {
+	// fmt.Printf("key:%s, keyPath:%s\n", key, keyPath)
+	if value.IsObject() {
+		value.ForEach(func(key, value j.Result) bool {
+			m.parseJSON(key, value, fmt.Sprintf("%s.%s", keyPath, key), newVal)
+			return true
+		})
+	} else if value.IsArray() {
+		for idx, name := range value.Array() {
+			tmpKeyPath := fmt.Sprintf("%s.%d", keyPath, idx)
+			if name.IsObject() {
+				name.ForEach(func(key, value j.Result) bool {
+					m.parseJSON(key, value, fmt.Sprintf("%s.%s", tmpKeyPath, key), newVal)
+					return true
+				})
+			} else {
+				v, _ := sj.Set(m.OriginJson, tmpKeyPath, name.String()+newVal)
+				m.rev = append(m.rev, v)
+				//fmt.Println(v)
+			}
+		}
+	} else {
+		v, _ := sj.Set(m.OriginJson, keyPath, value.String()+newVal)
+		m.rev = append(m.rev, v)
+		//fmt.Println(v)
+	}
+}
